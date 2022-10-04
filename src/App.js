9a3b5d670ea90dd671ea9bcfa0fd382119e2ad18
Dilmurodmirzakulov <dilmurodmirzakulovdev@gmail.com>
2022-10-04 19:47:12 +0500

    Creating ten rundom number array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Die from "./Die"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+const allNewDice = [];
+for (let i = 0; i < 10; i++) {
+  allNewDice.push(Math.ceil(Math.random()*6))
 }
+console.log(allNewDice)
 
-export default App;
+
+
+
+    return (
+        <main>
+            <div className="dice-container">
+                <Die value="1" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+                <Die value="5" />
+                <Die value="6" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+            </div>
+        </main>
+    )
+}
